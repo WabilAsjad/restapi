200bd74f6b178c9e40e22479e14b2142b3cfe760
wabil0 <49084510+wabil0@users.noreply.github.com>
2019-10-20 12:04:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,58 +6,59 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
 
-#Initilize app
+#Initialize app
 
 app = Flask(__name__)
 
 baseDir = os.path.abspath(os.path.dirname(__file__))
 
 #Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(baseDir, 'db.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(baseDir, 'database.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #Initial Database
-db = SQLAlchemy(app)
+database = SQLAlchemy(app)
 
 #Initial Marshmallow
 ma = Marshmallow(app)
 
 #Employee Class
-class Employee(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), unique = True)
-    job = db.Column(db.String(200))
-    school = db.Column(db.String(100))
-    age = db.Column(db.Integer)
+class Employee(database.Model): 
+    name = database.Column(database.String(100), unique = True)
+    job = database.Column(database.String(200))
+    identity = database.Column(database.Integer, primary_key = True)
+    school = database.Column(database.String(100))
+    age = database.Column(database.Integer)
     
-    def __init__(self, name, job, school, age):
+    def __init__(self, name, job, identity, school, age):
         self.name = name
         self.job = job
+        self.identity = identity
         self.school = school
         self.age = age
         
-
 #Employee Schema
 class EmployeeSchema(ma.Schema):
     class Meta:
-        fields = ('name', 'job', 'school', 'age')
+        fields = ('name', 'job', 'identity', 'school', 'age')
 
-# Initialize Schema
+# Initialize Schema for database
 employee_schema = EmployeeSchema()
 employees_schema = EmployeeSchema(many = True)
 
-# Create an Employee
+# Create an Employee 
 @app.route('/employee', methods = ['POST'])
 def add_Employee():
     name = request.json['name']
     job = request.json['job']
-    id = request.json['id']
+    identity = request.json['identity']
     school = request.json['school']
     age = request.json['age']
     
-    new_employee = Employee(name, job, id, school, age)
-    db.session.add(new_employee)
-    db.session.commit()
+    #Add employee to database and return in json format
+    new_employee = Employee(name, job, identity, school, age)
+    database.session.add(new_employee)
+    database.session.commit()
     return employee_schema.jsonify(new_employee)
 
 #Get All Employees
@@ -67,43 +68,41 @@ def get_employees():
     result = employees_schema.dump(all_employees) #Multiple employees
     return jsonify(result.data)
     
-    
-# Get Single Employee
-@app.route('/employee/<id>', methods=['GET'])
-def get_employee(id):
-    employee = Employee.query.get(id)
+#Get Single Employee
+@app.route('/employee/<identity>', methods=['GET'])
+def get_employee(identity):
+    employee = Employee.query.get(identity)
     return employee_schema.jsonify(employee)
 
-# Update an Employee
-@app.route('/employee/<id>', methods=['PUT'])
-def update_employee(id):
-    employee = Employee.query.get(id)
+#Update an Employee in database
+@app.route('/employee/<identity>', methods=['PUT'])
+def update_employee(identity):
+    employee = Employee.query.get(identity)
 
     name = request.json['name']
     job = request.json['job']
-    id = request.json['id']
+    identity = request.json['identity']
     school = request.json['school']
-    age = request.json['age']]
+    age = request.json['age']
 
     employee.name = name
     employee.job = job
-    employee.id = id
+    employee.id = identity
     employee.school = school
     employee.age = age
 
-    db.session.commit()
+    database.session.commit()
 
     return employee_schema.jsonify(employee)
 
-# Delete Employee
-@app.route('/product/<id>', methods=['DELETE'])
+#Delete Employee from database
+@app.route('/employee/<name>', methods=['DELETE'])
 def delete_employee(name):
     employee = Employee.query.get(name)
-    db.session.delete(employee)
-    db.session.commit()
+    database.session.delete(employee)
+    database.session.commit()
 
     return employee_schema.jsonify(employee)
-
 
 #Run server
 if __name__ == "__main__":
